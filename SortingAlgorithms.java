13c9e3feaa3ad28aa38277026b206f3fe8e9b776
d13405040-boop <d13405040@ems.npu.edu.tw>
2026-03-11 17:12:18 +0800

    Add files via upload

---
+++ SortingAlgorithms.java
@@ -0,0 +1,120 @@
+ipublic class SortingAlgorithms {
+
+    public static void main(String[] args) {
+        int[] data = {5, 2, 9, 1, 5, 6};
+        
+        System.out.println("=== Original Array ===");
+        printArray(data);
+        
+        System.out.println("\n=== Quick Sort ===");
+        int[] data1 = {5, 2, 9, 1, 5, 6};
+        quickSort(data1, 0, data1.length - 1);
+        printArray(data1);
+        
+        System.out.println("\n=== Bubble Sort ===");
+        int[] data2 = {5, 2, 9, 1, 5, 6};
+        bubbleSort(data2);
+        printArray(data2);
+        
+        System.out.println("\n=== Merge Sort ===");
+        int[] data3 = {5, 2, 9, 1, 5, 6};
+        mergeSort(data3, 0, data3.length - 1);
+        printArray(data3);
+        
+        System.out.println("\n=== Insertion Sort ===");
+        int[] data4 = {5, 2, 9, 1, 5, 6};
+        insertionSort(data4);
+        printArray(data4);
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
